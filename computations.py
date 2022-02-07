f34b5ccbe0399a6dfdd01d4fede19c97bb6375ff
DaliaDiana <98755926+DaliaDiana@users.noreply.github.com>
2022-02-07 17:54:32 +0200

    initial commit
    We are adding computations and main file. Computations will contain all the formulas for experiment

---
+++ computations.py
@@ -0,0 +1,18 @@
+def sum(x,y):
+    return x+y 
+
+def experiment_formula():
+    x1 = pow(1254.968844, 36)
+    print(x1)
+    x2 = pow(1334.38829, 80)
+    print(x2)
+    x3 = pow(293.1129, 93)
+    print(x3)
+    x4 = pow(8239.1292, 13)
+    print(x4)
+    x5 = pow(932.92928, 47)
+    print(x5)
+    print(x1-x2)
+    print(x1-x5+x3)
+    
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from computations import sum
+from computations import experiment_formula
+#d=sum(1,2)
+#print(d)
+
+experiment_formula()

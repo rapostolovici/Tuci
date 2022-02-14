2deabcee321a1079758baeb5fa09912f06833ad8
DaliaDiana <98755926+DaliaDiana@users.noreply.github.com>
2022-02-14 19:16:45 +0200

    Update main.py
    Added display message

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 from computations import sum
 from computations import experiment_formula
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+red = (255, 0, 0)
+blue = (0, 0, 255)
+green = (0, 255, 0)
+white = (255, 255, 255)
+yellow = (255, 255, 0)
+
+sense.show_letter("S", red)
+sleep(1)
+sense.show_letter("t", blue)
+sleep(1)
+sense.show_letter("a", green)
+sleep(1)
+sense.show_letter("r", white)
+sleep(1)
+sense.show_letter("t", yellow)
+sleep(1)
+sense.show_letter("i", red)
+sleep(1)
+sense.show_letter("n", red)
+sleep(1)
+sense.show_letter("g", red)
+sleep(1)
 #d=sum(1,2)
 #print(d)
 
 experiment_formula()
+

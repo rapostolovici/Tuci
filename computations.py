2134cfa74112a6efb36391f41dc450fd128ba036
AleGanyu <98968895+AleGanyu@users.noreply.github.com>
2022-02-14 18:02:58 +0200

    Update computations.py
    Extended computation

--- computations.py
+++ computations.py
@@ -1,18 +1,23 @@
+from numpy import *
+
 def sum(x,y):
     return x+y 
 
 def experiment_formula():
     x1 = pow(1254.968844, 36)
-    print(x1)
+    #print(x1)
     x2 = pow(1334.38829, 80)
-    print(x2)
+    #print(x2)
     x3 = pow(293.1129, 93)
-    print(x3)
+    #print(x3)
     x4 = pow(8239.1292, 13)
-    print(x4)
+    #print(x4)
     x5 = pow(932.92928, 47)
-    print(x5)
-    print(x1-x2)
-    print(x1-x5+x3)
+    x6=pow(47487577,464)
+    #print(x5)
+    r1=double(x1)-double(x2)/double(x4)+double(x6)
+    r2=double(x1)-double(x5)+double(x3)
+    print(r1)
+    print(r2)
     
 
